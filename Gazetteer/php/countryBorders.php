8e450563cca9f96b047d9b332771e479769c1604
KimJongUnicorn <77113954+KimJongUnicorn@users.noreply.github.com>
2021-01-20 13:36:21 +0000

    Add files via upload
    Updated countryBorders.php - added comments

--- Gazetteer/php/countryBorders.php
+++ Gazetteer/php/countryBorders.php
@@ -7,6 +7,7 @@
     
     $countriesList=array();
     $coordinates=array();
+    $isoCodes=array();
 
     foreach ($countries as $country) {
         array_push($countriesList, $country['properties']['name']);
@@ -16,6 +17,13 @@
         array_push($coordinates, $country['geometry']['coordinates']);
     }
 
-    $countriesCoords = array_combine ($countriesList, $coordinates);
+    foreach ($countries as $country) {
+        array_push($isoCodes, $country['properties']['iso_a3']);
+    }
+
+    //below is an array of iso codes and coordinates as key: value pairs
+    $countriesCoords = array_combine ($isoCodes, $coordinates);
+    //below is an array of iso codes and country names as key: value pairs
+    $countriesNames = array_combine ($isoCodes, $countriesList);
     
 ?>
